d4fbb2f2f8887ff068cae5ec9706b364784f17a4
Resmi Novianti <novianti.resmi@gmail.com>
2013-06-19 19:23:10 +0700

    Update 3Dmasjid_al-irsyad.cpp
    menambah data include

--- 3Dmasjid_al-irsyad.cpp
+++ 3Dmasjid_al-irsyad.cpp
@@ -1,4 +1,9 @@
 //masjid al-irsyad
 
-#include
-#include
+#include <windows.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include <stdarg.h> 
+#include <glut.h>
+#include <glu.h>
